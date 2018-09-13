8ffa04444d25339f5d4f70ba8fcd80b69df8a981
Viktor Uhryn <vik.ugrin@gmail.com>
2018-09-13 09:58:26 +0200

    improve physics and graphics

--- src/app/objects/house.js
+++ src/app/objects/house.js
@@ -1,9 +1,9 @@
 function House(position, t, front) {
 	const gList = [
-		[[[14,22,13,58,56,58,55,22],"#fff","",1],[[0,21,70,23,35,0],"#fff","",1],[[54,42,45,42,45,57,53,57],"#fff","",1],[[22,30,22,49,39,49,39,31],"#fff","",1],[[24,32,24,39,30,39,30,33],"#fff","",1],[[32,33,32,39,37,38,37,33],"#fff","",1],[[24,41,24,47,29,47,29,41],"#fff","",1],[[32,41,32,46,38,46,37,40],"#fff","",1]]
+		[[[0,66,17,46,30,29,39,15,48,0,58,15,68,32,80,48,95,67],"","camp1",1],[[48,1,48,66,65,66],"","camp2",1]]
 	];
 	const gListSizes = [
-		[70, 58]
+		[95, 67]
 	];
 	const type = typeof t === 'undefined' ? rInt(0, gList.length) : t;
 	const anim = new Anim(gList[type], []);
@@ -18,9 +18,6 @@ function House(position, t, front) {
 	this.r = () => {
 		c.save();
 		c.translate(position.x, gc.res.y - position.y - (gListSizes[type][1] / 2));
-		// c.scale(3, 3);
-		// c.lineWidth = 3;
-		// c.lineJoin = "round";
 		draw.r(anim.n(), gListSizes[type]);
 		c.restore();
 	};
--- src/app/objects/objects.js
+++ src/app/objects/objects.js
@@ -2,7 +2,9 @@ window.objects = (() => {
 	let list = [];
 
 	function addStaticObjects() {
-		// list.push(new House(new Vector(670, 0), 0));
+		list.push(new House(new Vector(670, 0), undefined, false));
+		list.push(new Owl(new Vector(670, 75)));
+
 		list.push(new Campfire(new Vector(450, 0)));
 		list.push(new Campfire(new Vector(8350, 3000)));
 		list.push(new Campfire(new Vector(24350, 6500)));
@@ -13,7 +15,7 @@ window.objects = (() => {
 		});
 
 		[100, 140, 200, 580, 850, 940, 8100, 8180, 8450, 8600, 24050, 24200, 24480, 24640].forEach((item) => {
-			list.push(new Tree(new Vector(item, mountain.getHeight(item) - 5), rInt(0, 2)));
+			list.push(new Tree(new Vector(item, mountain.getHeight(item) - 5), 1));
 		});
 	}
 
@@ -21,19 +23,20 @@ window.objects = (() => {
 		i: () => {
 			const trip = mountain.getTrip();
 			trip.filter((item) => item.type !== 'hole' && item.type !== 'camp').forEach((item) => {
+				if (item.start.x > 1000 && item.start.x < 10000) {
+					if (!rInt(0, 5)) {
+						const x = rInt(item.start.x, item.end.x);
+						const y = (item.start.y + 5) + (((item.end.y + 5) - (item.start.y + 5)) * ((x - item.start.x) / (item.end.x - item.start.x)));
+						list.push(new Bush(new Vector(x, y)));
+					}
+				}
+
 				if (item.start.x > 1000 && item.start.x < 36000) {
 					const countTree = Math.floor(rFloat(0, 1 + 2 * (1 - (item.start.x / 36000))));
 					for (let i = 0; i < countTree; i++) {
 						const x = rInt(item.start.x, item.end.x);
 						const y = (item.start.y + 5) + (((item.end.y + 5) - (item.start.y + 5)) * ((x - item.start.x) / (item.end.x - item.start.x)));
 						list.push(new Tree(new Vector(x, y)));
-					}
-				}
-				if (item.start.x > 1000 && item.start.x < 10000) {
-					if (!rInt(0, 5)) {
-						const x = rInt(item.start.x, item.end.x);
-						const y = (item.start.y + 5) + (((item.end.y + 5) - (item.start.y + 5)) * ((x - item.start.x) / (item.end.x - item.start.x)));
-						list.push(new Bush(new Vector(x, y)));
 					}
 				}
 			});
@@ -52,7 +55,7 @@ window.objects = (() => {
 							(!front && !item.front || front && item.front);
 					} else {
 						return item.position.x >= camera.getView().start.x && item.position.x <= camera.getView().end.x &&
-							(item instanceof Campfire || item instanceof Bench);
+							(item instanceof Campfire || item instanceof Bench || item instanceof House);
 					}
 				})
 				.forEach((item) => {
